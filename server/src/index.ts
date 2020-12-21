a777f1bb970e2e04deabf67a578363968b5f1d44
Luiz Baldi <baldilp@gmail.com>
2020-12-21 10:48:25 -0300

    chore: add logs for missing environment variables

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,48 @@ dotenv.config();
 
 const app = express();
 
-const { PORT = 4000, FRONTEND_URL: frontendUrl = "" } = process.env;
+const {
+  PORT = 4000,
+  FRONTEND_URL: frontendUrl = "",
+  SPOTIFY_CLIENT_ID: spotifyClientId,
+  SPOTIFY_CLIENT_SECRET: spotifyClientSecret,
+  YOUTUBE_CLIENT_ID: youtubeClientId,
+  YOUTUBE_SECRET_KEY: youtubeSecretKey
+} = process.env;
+
+(function checkCredentials() {
+  const hasCredentials = {
+    spotify: true,
+    youtube: true
+  };
+
+  if (!spotifyClientId || !spotifyClientSecret) {
+    hasCredentials.spotify = false;
+  }
+
+  if (!youtubeClientId || !youtubeSecretKey) {
+    hasCredentials.youtube = false;
+  }
+
+  if (!hasCredentials.spotify && !hasCredentials.youtube) {
+    console.log(
+      "Please configure your environment variables according to our CONTRIBUTING.md guide."
+    );
+    return;
+  }
+
+  if (!hasCredentials.spotify) {
+    console.log(
+      "Spotify credentials not properly configured, please check your server/.env file."
+    );
+  }
+
+  if (!hasCredentials.youtube) {
+    console.log(
+      "Youtube credentials not properly configured, please check your server/.env file."
+    );
+  }
+})();
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", [frontendUrl]);
